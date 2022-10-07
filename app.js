8b94f70ea06458b5f31b542587877260461d1c6a
SON <wjdgh2777@naver.com>
2022-10-07 22:09:34 +0900

    Feat: 연결표시

--- app.js
+++ app.js
@@ -13,13 +13,36 @@ const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
     // 연결이 이루어지면 그에 관련된 정보가 socket에 담겨짐
+    console.log(socket.id + ": Connection");
+
     socket.on("chatting", (data) => {
         const { name, msg } = data;
         io.emit("chatting", {
+            // 접속된 모든 클라이언트에게 메시지를 전송한다
             name: name,
             msg: msg,
             time: moment(new Date()).format("h:mm A"),
         });
+    });
+
+    socket.on("disconnect", () => {
+        console.log(socket.id + ": Disconnection");
+        clearInterval(socket.interval);
+    });
+
+    // 클라이언트로 메세지 보내기
+    // socket.interval = setInterval(() => {
+    //     // 1초마다 클라이언트로 메시지 전송
+    //     socket.emit("chatting", {
+    //         name: "Server",
+    //         msg: "Hello have a nice day!",
+    //         time: moment(new Date()).format("h:mm A"),
+    //     });
+    // }, 1000);
+
+    // 에러 시
+    socket.on("error", (error) => {
+        console.error(error);
     });
 });
 
